ecbe2a4db9358d8c51b91d68ddeb8f56bccdb226
Silva <adsonplaygg@gmail.com>
2022-01-27 21:15:33 -0300

    Moving pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,30 @@
 package application;
 
-import bordgame.Board;
-import bordgame.Position;
+import java.util.Scanner;
+
 import chess.ChessMath;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
 		ChessMath chessMatch = new ChessMath();
-		UI.printBoard(chessMatch.getPieces());
-	}
 
+		while (true) {
+			UI.printBoard(chessMatch.getPieces());
+			System.out.println();
+			System.out.println("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.println("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			
+		}
+	}
 }
