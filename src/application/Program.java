a6077335229fe127732219636d60e45e47d9485e
Silva <adsonplaygg@gmail.com>
2022-01-30 10:14:30 -0300

    Printing possible moves

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.ChessMath;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -14,17 +16,32 @@ public class Program {
 		ChessMath chessMatch = new ChessMath();
 
 		while (true) {
-			UI.printBoard(chessMatch.getPieces());
-			System.out.println();
-			System.out.println("Source: ");
-			ChessPosition source = UI.readChessPosition(sc);
-			
-			System.out.println();
-			System.out.println("Target: ");
-			ChessPosition target = UI.readChessPosition(sc);
-			
-			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-			
+			try {
+					UI.clearScreen();
+					UI.printBoard(chessMatch.getPieces());
+					System.out.println();
+					System.out.print("Source: ");
+					ChessPosition source = UI.readChessPosition(sc);
+					
+					boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+					UI.clearScreen();
+					UI.printBoard(chessMatch.getPieces(), possibleMoves);
+					System.out.println();
+					System.out.print("Target: ");
+					ChessPosition target = UI.readChessPosition(sc);
+
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			}
+			catch(ChessException e ) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+
+			}
+			catch(InputMismatchException e ) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+
+			}
 		}
 	}
 }
--- src/chess/ChessMath.java
+++ src/chess/ChessMath.java
@@ -26,6 +26,12 @@ public class ChessMath {
 		return mat;
 	}
 
+	public boolean[][] possibleMoves(ChessPosition sourcePosition) {
+		Position positon = sourcePosition.toPosition();
+		validateSourcePosition(positon);
+		return board.piece(positon).possibleMoves();
+	}
+	
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
